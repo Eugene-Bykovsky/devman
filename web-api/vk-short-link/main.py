1dece978300383bb7eb0237bcd47b8ddcc7e9110
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2024-10-15 04:00:00 +0400

    add code for vk api getShortLink

--- web-api/vk-short-link/main.py
+++ web-api/vk-short-link/main.py
@@ -5,14 +5,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-URL = 'https://api.vk.ru/method/utils.getServerTime'
-TOKEN = os.getenv('SERVICE_TOKEN')
-PAYLOAD = {
-    'access_token': TOKEN,
-    'v': 5.199,
+VK_API_URL = 'https://api.vk.ru/method/'
+VK_API_METHODS = {
+    'getShortLink': 'utils.getShortLink',
 }
+TOKEN = os.getenv('SERVICE_TOKEN')
 
-response = requests.get(URL, params=PAYLOAD)
+
+def shorten_link(token, url):
+    response = requests.get(
+        f'{VK_API_URL+VK_API_METHODS["getShortLink"]}?access_token={token}&v=5'
+        f'.199&url={url}')
+    return response.json()['response']['short_url']
+
 
 if __name__ == '__main__':
-    print(response.text)
+    long_url = input('Введите ссылку: ')
+    print('Сокращенная ссылка: ', shorten_link(TOKEN, long_url))
+
+# https://dvmn.org/modules
