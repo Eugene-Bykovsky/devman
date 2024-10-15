5c37ae3160d0a65bafd2acde4615e94c0daaa78e
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2024-10-15 23:05:11 +0400

    code fix

--- web-api/vk-short-link/main.py
+++ web-api/vk-short-link/main.py
@@ -14,50 +14,75 @@ VK_API_METHODS = {
 TOKEN = os.getenv('SERVICE_TOKEN')
 
 
-def vk_api_request(method, params):
+def get_link_key(url: str) -> str:
+    """Достает из ссылки часть URL после «/»"""
+    return urlparse(url).path.replace('/', '')
+
+
+def vk_api_request(method: str, params: dict) -> dict:
+    """Делает запрос в API VK"""
     response = requests.get(f'{VK_API_URL + VK_API_METHODS[method]}',
                             params=params)
     response.raise_for_status()
     if 'error' in response.json():
         err_code = response.json()['error']['error_code']
         err_msg = response.json()['error']['error_msg']
-        raise ValueError(f'code {err_code} - {err_msg}')
-    return response.json()['response']
+        raise ValueError(f'Ошибка в методе - {method}. '
+                         f'Code {err_code} - {err_msg}')
+    return response.json()
 
 
-def shorten_link(token, link):
+def is_shorten_link(token: str, url: str) -> bool:
+    """Проверяем, является ли ссылка сокращенной через запрос к API."""
+    link_key = get_link_key(url)
     params = {
         'access_token': token,
         'v': '5.199',
-        'url': link
-    }
-    return vk_api_request('getShortLink', params)['short_url']
-
-
-def count_clicks(token, link):
-    link = urlparse(link).path.replace('/', '')
-    params = {
-        'access_token': token,
-        'v': '5.199',
-        'key': link,
+        'key': link_key,
         'interval': 'forever'
     }
-    return vk_api_request('getLinkStats', params)['stats'][0]['views']
+    try:
+        vk_api_request('getLinkStats', params)
+        return True
+    except ValueError:
+        return False
+
+
+def shorten_link(token: str, url: str) -> str:
+    """Преобразует длинную ссылку в короткую с помощью API VK"""
+    params = {
+        'access_token': token,
+        'v': '5.199',
+        'url': url
+    }
+    return vk_api_request('getShortLink', params)['response']['short_url']
+
+
+def count_clicks(token: str, url: str) -> int:
+    """Возвращает количество кликов по ссылке с помощью API VK"""
+    link_key = get_link_key(url)
+    params = {
+        'access_token': token,
+        'v': '5.199',
+        'key': link_key,
+        'interval': 'forever'
+    }
+    return vk_api_request('getLinkStats', params)['response']['stats'][0][
+        'views']
 
 
 if __name__ == '__main__':
-    long_link = input('Введите ссылку: ')
+    link = input('Введите ссылку: ')
     try:
-        short_link = shorten_link(TOKEN, long_link)
-        count_clicks = count_clicks(TOKEN, short_link)
-        print('Сокращенная ссылка: ', short_link)
-        print('Количество кликов по ссылке: ', count_clicks)
+        if is_shorten_link(TOKEN, link):
+            count_clicks = count_clicks(TOKEN, link)
+            print('Количество кликов по ссылке: ', count_clicks)
+        else:
+            short_link = shorten_link(TOKEN, link)
+            print('Сокращенная ссылка: ', short_link)
     except requests.exceptions.HTTPError as http_err:
         print(f"Can't get data from server.HTTP error:\n{http_err}")
     except requests.exceptions.RequestException as req_err:
         print(f"Can't get data from server. Request error:\n {req_err}")
     except ValueError as api_err:
         print(f'VK API error: {api_err}')
-
-# https://dvmn.org/modules
-# https://vk.cc/cvPDMl
